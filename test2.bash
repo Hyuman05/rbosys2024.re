f82533cb0c6434654c913b03a4749fd9d7c28d0d
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2024-11-28 19:09:24 +0900

    Add a o

--- test2.bash
+++ test2.bash
@@ -2,36 +2,36 @@
 #SPDX-FileCopyrightText: 2024 Hyuta Sasaki 
 #SPDX-Licence-Indentifier: BSD-3-Clause
 ng () {
-        echo NG at Line ${1}
-	ret=1
+    echo NG at Line ${1}
+    ret=1
 }
 
 ret=0
 
 #正しいインプット
 out=$(./plus_omikuji 1/1)
-[ "${out}" = 大吉 ] || [ "${out}" = 大凶 ] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "大凶" ] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/2)
-[ "${out}" = 大吉 ] || [ "${out}" = 吉 ] || [ "${out}" = 中吉 ] || [ "${out}" = 小吉 ] || [ "${out}" = 末吉 ] || [ "${out}" = 凶] || [ "${out}" = 大凶] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "吉" ] || [ "${out}" = "中吉" ] || [ "${out}" = "小吉" ] || [ "${out}" = "末吉" ] || [ "${out}" = "凶" ] || [ "${out}" = "大凶"] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/3)
-[ "${out}" = 大吉 ] || ["${out}" = 凶] || ["${out}" = 大凶] || ng ${LINENO}
+[ "${out}" = "大吉" ] || ["${out}" = "凶" ] || ["${out}" = "大凶" ] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/4)
-[ "${out}" = 大吉 ] || [ "${out}" = 吉 ] || [ "${out}" = 中吉 ] || [ "${out}" = 小吉 ] || [ "${out}" = 末吉 ] || [ "${out}" = 凶] || [ "${out}" = 大凶] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "吉" ] || [ "${out}" = "中吉" ] || [ "${out}" = "小吉" ] || [ "${out}" = "末吉" ] || [ "${out}" = "凶" ] || [ "${out}" = "大凶" ] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/5)
-[ "${out}" = 大吉 ] || [ "${out}" = 吉 ] || [ "${out}" = 中吉 ] || [ "${out}" = 小吉 ] || [ "${out}" = 末吉 ] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "吉" ] || [ "${out}" = "中吉" ] || [ "${out}" = "小吉" ] || [ "${out}" = "末吉" ] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/6)
-[ "${out}" = 大吉 ] || [ "${out}" = 吉 ] || [ "${out}" = 中吉 ] || [ "${out}" = 小吉 ] || [ "${out}" = 末吉 ] || [ "${out}" = 凶] || [ "${out}" = 大凶] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "吉" ] || [ "${out}" = "中吉" ] || [ "${out}" = "小吉" ] || [ "${out}" = "末吉" ] || [ "${out}" = "凶" ] || [ "${out}" = "大凶" ] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/7)
-[ "${out}" = 大吉 ] || [ "${out}" = 吉 ] || [ "${out}" = 中吉 ] || [ "${out}" = 小吉 ] || [ "${out}" = 末吉 ] || [ "${out}" = 凶] || [ "${out}" = 大凶] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "吉" ] || [ "${out}" = "中吉" ] || [ "${out}" = "小吉" ] || [ "${out}" = "末吉" ] || [ "${out}" = "凶" ] || [ "${out}" = "大凶" ] || ng ${LINENO}
 
 out=$(./plus_omikuji 1/8)
-[ "${out}" = 大吉 ] || [ "${out}" = 吉 ] || [ "${out}" = 中吉 ] || [ "${out}" = 末吉 ] ||　[ "${out}" = 凶] || [ "${out}" = 大凶] || ng ${LINENO}
+[ "${out}" = "大吉" ] || [ "${out}" = "吉" ] || [ "${out}" = "中吉" ] || [ "${out}" = "末吉" ] ||　[ "${out}" = "凶" ] || [ "${out}" = "大凶" ] || ng ${LINENO}
 
 #変なインプット
 
