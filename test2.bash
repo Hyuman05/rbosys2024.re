3d6b916b327401d19ae8074f46334a0a3437fa2a
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2024-11-29 00:22:08 +0900

    Add a omik

--- test2.bash
+++ test2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Hyuta Sasaki
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-Licence-Identifier: BSD-3-Clause
 
 ng() {
     echo "NG at Line ${1}"
@@ -9,42 +9,42 @@ ng() {
 
 res=0
 
-# 正しい入力
-out=$(./plus_omikuji 1/1)
+#　正しい入力
+out=$(echo 1/1 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 1/2)
-[ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
+out=$(echo 1/2 |./plus_omikuji)
+[ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は:小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 1/3)
+out=$(echo 1/3 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 1/4)
+out=$(echo 1/4 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 1/5)
+out=$(echo 1/5 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 1/6)
+out=$(echo 1/6 |./plus_omikuji)
+[ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO} 
+
+out=$(echo 1/7 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 1/7)
-[ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 小吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
-
-out=$(./plus_omikuji 1/8)
+out=$(echo 1/8 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 吉" ] || [ "${out}" = "今日の運勢は: 中吉" ] || [ "${out}" = "今日の運勢は: 末吉" ] || [ "${out}" = "今日の運勢は: 凶" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 01/01)
+out=$(echo 01/01 |./plus_omikuji)
 [ "${out}" = "今日の運勢は: 大吉" ] || [ "${out}" = "今日の運勢は: 大凶" ] || ng ${LINENO}
 
 # 異常な入力
-out=$(./plus_omikuji)
-[ "${out}" = "使用方法: ./plus_omikuji MM/DD" ] || ng ${LINENO}
-
-out=$(./plus_omikuji あ)
+out=$(echo  |./plus_omikuji)
 [ "${out}" = "無効な入力です。MM/DDの形式で入力してください。" ] || ng ${LINENO}
 
-out=$(./plus_omikuji 13/40)
+out=$(echo あ |./plus_omikuji)
+[ "${out}" = "無効な入力です。MM/DDの形式で入力してください。" ] || ng ${LINENO}
+
+out=$(echo 13/32 |./plus_omikuji)
 [ "${out}" = "月は1~12、日にちは1~31の範囲で入力してください。" ] || ng ${LINENO}
 
 
